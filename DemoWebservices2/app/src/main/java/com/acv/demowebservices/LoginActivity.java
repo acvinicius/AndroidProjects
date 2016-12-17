7b79ab579f0728e1c25e2d4fc617453b18c0d4d3
Vinicius Castro <vinicius.castro@docusign.com>
2016-12-17 12:08:50 -0200

    novos projetos

--- DemoWebservices2/app/src/main/java/com/acv/demowebservices/LoginActivity.java
+++ DemoWebservices2/app/src/main/java/com/acv/demowebservices/LoginActivity.java
@@ -3,9 +3,28 @@ package com.acv.demowebservices;
 import android.app.Activity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +48,13 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_logar)
     public void logar() {
+        if (isConnected()) {
+            new LoginTask().execute("http://192.168.43.156:1000/login");
+        } else {
+            Toast.makeText(LoginActivity.this,
+                    "Você não está conectado",
+                    Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -37,4 +63,115 @@ public class LoginActivity extends AppCompatActivity {
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
     }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+
+        String usuarioET;
+        String senhaET;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            usuarioET = etUsuario.getText().toString();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            try {
+                URL url = new URL(strings[0]);
+
+                JSONObject usuario = new JSONObject();
+                usuario.put("usuario", usuarioET);
+//                usuario.put("senha", etSenha);
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(15000);
+                httpURLConnection.setConnectTimeout(15000);
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+
+                OutputStream os = httpURLConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(usuario));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=httpURLConnection.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            httpURLConnection.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+
+            } catch (MalformedURLException mue) {
+                result = mue.getMessage();
+            } catch (JSONException jse) {
+                result = jse.getMessage();
+            } catch (IOException ioe) {
+                result = ioe.getMessage();
+            } catch (Exception e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute (String s) {
+            super.onPostExecute(s);
+            Toast.makeText(LoginActivity.this,
+                    s,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while(itr.hasNext()){
+
+                String key= itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
+    }
+
 }
